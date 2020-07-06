5a074ea8972e68632b64cd17a5857834c762d8f8
Scott Horn (Corvel) <shorn-work@srhsoftware.com>
2020-07-06 10:42:18 -0700

    Updated LoadScripts in psm1 files

--- Source/Corvel.DCOps.LocalSettings.psm1
+++ Source/Corvel.DCOps.LocalSettings.psm1
@@ -17,16 +17,22 @@ if (-not (Test-Path $ModuleRoot)) {
     $ModuleRoot = Split-Path $ModulePath
 }
 # Load and export public functions
-$Scripts = Get-ChildItem -Path "$ModuleRoot\public" -Filter "*.ps1"
-foreach ($ScriptFile in $Scripts) {
-    . $ScriptFile.FullName
-    $ExportedFunctions += $ScriptFile.BaseName
+if (Test-Path "$ModuleRoot\public") {
+    $Scripts = Get-ChildItem -Path "$ModuleRoot\public" -Filter "*.ps1"
+    foreach ($ScriptFile in $Scripts) {
+        . $ScriptFile.FullName
+        $ExportedFunctions += $ScriptFile.BaseName
+    }
 }
 
 # Load private functions
-$Scripts = Get-ChildItem -Path "$ModuleRoot\private" -Filter '*.ps1'
-foreach ($ScriptFile in $Scripts) {
-    . $ScriptFile.FullName
+if (Test-Path "$ModuleRoot\private") {
+    $Scripts = Get-ChildItem -Path "$ModuleRoot\private" -Filter '*.ps1'
+    foreach ($ScriptFile in $Scripts) {
+        . $ScriptFile.FullName
+    }
 }
 Export-ModuleMember -Function $ExportedFunctions
 #endregion LoadScripts
+
+
