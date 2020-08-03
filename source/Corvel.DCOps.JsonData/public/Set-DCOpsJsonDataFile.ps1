55dfea3f00f40fd322fd8fb44221372ff244c7de
Scott Horn (Corvel) <shorn-work@srhsoftware.com>
2020-08-03 13:17:50 -0700

    Added check for file access

--- source/Corvel.DCOps.JsonData/public/Set-DCOpsJsonDataFile.ps1
+++ source/Corvel.DCOps.JsonData/public/Set-DCOpsJsonDataFile.ps1
@@ -12,26 +12,28 @@ function Set-DCOpsJsonDataFile {
       [ValidateNotNullOrEmpty()]
       [string]$DCOpServer = (Get-DCOpsLocalSetting -Name 'dcopserver')
    )
-   $AlwaysConfirm = $false
-
+   $Caller = (Get-PSCallStack)[1].Command
    if (-not ($JsonDataShare = Get-DCOpsJsonDataShare -DCOpServer $DCOpServer)) {
       throw "Unable to locate DCOps Host and/or Data Share"
    }
    $FileName = [System.IO.Path]::GetFileNameWithoutExtension($Name)
+   if (-not (Get-DCOpsJsonDataFileAccess -Name $FileName -Caller $Caller -Access Write)) {
+      throw "Permission denied to Json Data file $FileName"
+      return
+   }
+
    $FilePath = "$JsonDataShare\$($FileName).json"
    $BackupFilePath = "$JsonDataShare\$($FileName).bak"
    if ($FileName -eq 'dcopscredentials') {
       $AlwaysConfirm = $true
    }
    try {
-      if ($PSCmdlet.ShouldProcess($FileName, 'Saving Json data file')) {
-         if ($AlwaysConfirm) {
-            if ($PSCmdlet.ShouldContinue("Confirm updating the shared credentials on the DCOps Server.", "Protected File")) {  
-               if (Test-Path $FilePath) {
-                  Copy-Item -LiteralPath $FilePath -Destination $BackupFilePath -Force
-               }
-               $InputObject | ConvertTo-Json | Set-Content -LiteralPath $FilePath -ErrorAction Stop
+      if ($PSCmdlet.ShouldProcess($FileName, 'Saving Json data file')) {         
+         if ($PSCmdlet.ShouldContinue("Update Json data file?.", "Protected File")) {  
+            if (Test-Path $FilePath) {
+               Copy-Item -LiteralPath $FilePath -Destination $BackupFilePath -Force
             }
+            $InputObject | ConvertTo-Json | Set-Content -LiteralPath $FilePath -ErrorAction Stop
          }
       }
    } catch {
