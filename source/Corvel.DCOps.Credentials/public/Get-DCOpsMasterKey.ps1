83be84d638957acbc6f4d9aba14bd98ee3e35110
Scott Horn <shorn-work@srhsoftware.com>
2020-08-31 16:06:23 -0700

    Fixed missing paren and adjusted get-dcopsmasterkey

--- source/Corvel.DCOps.Credentials/public/Get-DCOpsMasterKey.ps1
+++ source/Corvel.DCOps.Credentials/public/Get-DCOpsMasterKey.ps1
@@ -3,9 +3,13 @@ function Get-DCOpsMasterKey {
    [OutputType([securestring])]
    param ()
 
-   if (-not ($MasterKey = Get-DCOpsLocalSetting -Key 'dcopsmasterkey' | ConvertTo-SecureString -ErrorAction SilentlyContinue)) {
-      throw 'DCOps Master Key not found.'
-      return
+   $MasterKey = Get-DCOpsLocalSetting -Key 'dcopsmasterkey'
+   if ($MasterKey) {
+      $MasterKey = $MasterKey | ConvertTo-SecureString -ErrorAction SilentlyContinue
    }
+   # if (-not ($MasterKey = Get-DCOpsLocalSetting -Key 'dcopsmasterkey' | ConvertTo-SecureString -ErrorAction SilentlyContinue)) {
+   #    throw 'DCOps Master Key not found.'
+   #    return
+   # }
    return $MasterKey
 }
--- source/Corvel.DCOps.JsonData/private/Get-DCOpsJsonDataFileAccess.ps1
+++ source/Corvel.DCOps.JsonData/private/Get-DCOpsJsonDataFileAccess.ps1
@@ -35,7 +35,7 @@ function Get-DCOpsJsonDataFileAccess {
    $AccessList = $WebResponse.Content | ConvertFrom-Json | ConvertTo-HashTable
 
    # If the file isn't in the list, then grant access
-   if (-not ($AccessList.ContainsKey($Name)) { return $true}
+   if (-not ($AccessList.ContainsKey($Name))) { return $true}
    # If there isn't anything defined for the access being requested
    if (-not ($AccessList[$Name][$Access])){ $true }
    # If the caller is in the requested access list, grant access
