c26aafcf7a8b0984603a955799476f08541987a9
Scott Horn <shorn-work@srhsoftware.com>
2020-10-02 14:44:49 -0700

    Added Set-DCOpsServer

--- Corvel.DCops.Servers/public/Set-DCOpsServer.ps1
+++ Corvel.DCops.Servers/public/Set-DCOpsServer.ps1
@@ -1,21 +1,25 @@
 function Set-DCOpsServer {
-   [CmdletBinding()]
-   [OutputType([System.Collections.Generic.List[PSCustomObject]])]
+   [CmdletBinding(SupportsShouldProcess)]
+   [OutputType('DCOpsServer')]
    param (
-      [Parameter(Mandatory=$true)]
+      [Parameter(Mandatory=$true, ParameterSetName='Object', ValueFromPipeline)]
+      [PSTypeName('DCOpsServer')]$DCOpsServer,
+      [Parameter(Mandatory=$true, ParameterSetName='Details')]
       [ValidateScript({$_ -in (Get-DCOpsServerType)})]
       [string]$ServerType,
       [Parameter(Mandatory=$true, ParameterSetName='Details')]
       [string]$HostName,      
-      [Parameter(Mandatory=$true)]
+      [Parameter(Mandatory=$true, ParameterSetName='Details')]
       [string]$DisplayName,
-      [Parameter(Mandatory=$true)]
+      [Parameter(Mandatory=$true, ParameterSetName='Details')]
       [ValidateScript({$_ -in (Get-DCOpsEnvironment)})]
       [string]$Environment,
-      [Parameter(Mandatory=$true)]
+      [Parameter(Mandatory=$true, ParameterSetName='Details')]
       [ValidateScript({$_ -in (Get-DCOpsLocation)})]
       [string]$Location,
+      [Parameter(ParameterSetName='Details')]
       [System.Collections.Generic.List[string]]$IPAddress = @(),
+      [Parameter(ParameterSetName='Details')]
       [hashtable]$Custom = @{},
       [ValidateNotNullOrEmpty()]
       [string]$DCOpsHost = (Get-DCOpsLocalSetting -Name 'dcopshost')
@@ -24,20 +28,31 @@ function Set-DCOpsServer {
 
    $DCOpsServers = Import-DCOpsServerFile -DCOpsHost $DCOpsHost
    
-   $Params = @{
-      ServerType = $ServerType
-      HostName = $HostName
-      DisplayName = $DisplayName
-      Environment = $Environment
-      Location = $Location
+   if (-not ($PSBoundParameters.ContainsKey('DCOpsServer'))) {
+      $Params = @{
+         ServerType = $ServerType
+         HostName = $HostName
+         DisplayName = $DisplayName
+         Environment = $Environment
+         Location = $Location
+      }
+      if ($PSBoundParameters.ContainsKey('IPAddress')) { $Params['IPAddress'] = $IPAddress }  
+      if ($PSBoundParameters.ContainsKey('Custom')) { $Params['Custom'] = $Custom }
+      try {
+         $DCOpsServer = New-DCOpsServerObject @Params
+      } catch {
+         return
+      }
    }
-   if ($PSBoundParameters.ContainsKey('IPAddress')) { $Params['IPAddress'] = $IPAddress }  
-   if ($PSBoundParameters.ContainsKey('Custom')) { $Params['Custom'] = $Custom }
-   try {
-      $ServerObject = New-DCOpsServerObject @Params
-   } catch {
-      return
+
+   # See if we already have the object
+   $SearchObj = $DCOpsServers | Where-Object {$_.ServerType -eq $DCOpsServer.ServerType -and $_.HostName -eq $DCOpsServer.HostName}
+   if ($SearchObj) {
+      $DCOpsServers.Remove($SearchObj) | Out-Null
    }
-   
-   return $ServerObject
+   if ($PSCmdlet.ShouldProcess($DCOpsServer.DisplayName, "Updating servers")) {
+      $DCOpsServers.Add($DCOpsServer) | Out-Null
+      Set-DCOpsJsonDataFile -Name 'servers' -InputObject $DCOpsServers -DCOpsHost $DCOpsHost
+   }
+   return $DCOpsServer
 }
